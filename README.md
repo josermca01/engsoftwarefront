b5fe6565c0f236af8edb233806efcf231515e595
Jose Ricardo <josermca01@gmail.com>
2022-09-22 20:37:47 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Projeto base para aplicações **React, TypeScript e Tailwind**.
+
+Pacotes:
+
+- React
+  - react-icons
+  - react-spinners
+  - react-router-dom
+- TypeScript
+- Tailwindcss
+- Axios
+- Craco
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+export default function App() {
+  console.log('Hello Console!');
+
+  return (
+    <div>
+      <header className="fixed w-full">
+        <div className="bg-blue-500 p-4 flex justify-between">
+          <div className="text-center font-semibold text-xl">
+            Projeto React / TypeScript
+          </div>
+          <div className="flex">
+            <div className="mx-3 text-center font-semibold text-xl">
+              Avaliações
+            </div>
+            <div className="mx-3 text-center font-semibold text-xl">
+              Avaliações
+            </div>
+            <div className="mx-3 text-center font-semibold text-xl">
+              Avaliações
+            </div>
+            <div className="mx-3 text-center font-semibold text-xl">
+              Avaliações
+            </div>
+          </div>
+          <div className="text-center font-semibold text-xl">
+            Entrar
+          </div>
+        </div>
+      </header>
+
+      <main className="pt-20">
+        <div className="container mx-auto">
+          <h2>Hello World!</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
