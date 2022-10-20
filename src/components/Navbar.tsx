7b2f687885722eb52eff8628f5333d7d86306edb
joser <josermca01@gmail.com>
2022-10-20 16:57:02 -0300

    hero pronto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import {useState} from 'react'
+import { useState } from "react";
+import { navLinks } from "../constants";
 
 export const Navbar = () => {
+  const [active, setActive] = useState("Home");
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <h1 className='text-white text-xl mr-10'>Nome da Marca</h1>
-      <ul className='list-none sm:flex hidden justify-start items-center flex-1'>
-        <li className={`text-white font-poppins font-normal cursor-pointer text-[16px] mr-7`}>Home <a href="#Home"></a></li>
-        <li className={`text-white font-poppins font-normal cursor-pointer text-[16px] mr-7`}>Sobre <a href="#Home"></a></li>
-        <li className={`text-white font-poppins font-normal cursor-pointer text-[16px] mr-7`}>Avaliações <a href="#Home"></a></li>
-        <li className={`text-white font-poppins font-normal cursor-pointer text-[16px] mr-7`}>Contato <a href="#Home"></a></li>
+      <ul className="list-none sm:flex hidden justify-start items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
       </ul>
       <div className='text-white bg-blue-600 font-poppins font-normal cursor-pointer text-[16px] p-4 rounded-md'>Entre agora</div>
     </nav>
---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "sobre",
+    title: "Sobre",
+  },
+  {
+    id: "avaliações",
+    title: "Avaliações",
+  },
+  {
+    id: "contato",
+    title: "Contato",
+  },
+];
