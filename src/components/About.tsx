04463367d06046510232495a5ba1ec2ca08fc0e4
joser <josermca01@gmail.com>
2022-10-22 18:54:31 -0300

    Card pronto

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ export const About = () => {
     <section className="relative bg-white border-b py-8">
       <div className="container mx-auto flex flex-wrap pt-4 pb-12">
         <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Title
+          Sobre
         </h2>
         <div className="w-full mb-4">
           <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import Equipeimg from "../images/Equipeimg.png"
 
 export const AboutCard = () => {
   return (
   <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
   <div className="rounded-lg shadow-lg bg-white max-w-sm">
     <div className="p-6">
-      <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-      <p className="text-gray-700 text-base mb-4">
+      <div className="text-gray-900 text-xl font-medium mb-2 bg-blue-300 p-8 rounded-md flex items-center">
+        <div className="bg-white rounded-full p-4 mr-6">
+          <img src={Equipeimg} alt="" />
+        </div>
+        Nossa Equipe
+        </div>
+      <p className="text-gray-700 text-base mb-4 px-6">
         Some quick example text to build on the card title and make up the bulk of the card's
         content.
       </p>
